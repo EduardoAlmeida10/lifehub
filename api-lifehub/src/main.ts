9809e1c15cfc565934ac718cd2eb4d7ca81e8245
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-02-10 16:19:21 -0300

    chore: configuring cors

--- api-lifehub/src/main.ts
+++ api-lifehub/src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.URL_FRONTEND,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log('Servidor rodando!!!');
 }
+
 bootstrap();
