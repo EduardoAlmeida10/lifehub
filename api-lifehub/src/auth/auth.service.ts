4e21662fe0df0f54d46a0146ca7c194e070b13d9
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-02-12 18:20:58 -0300

    feat: login route completed.

---
+++ api-lifehub/src/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login-auth.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    return user;
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.validateUser(dto.email, dto.password);
+
+    const userPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    const token = this.jwtService.sign(userPayload);
+
+    return {
+      access_token: token,
+    };
+  }
+}
