ab7c5146286793a6291bfbbe3a04bad329b2444e
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-02-12 11:11:01 -0300

    feat: validationPipe

--- api-lifehub/src/main.ts
+++ api-lifehub/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   app.enableCors({
     origin: process.env.URL_FRONTEND,
@@ -12,7 +19,6 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log('Servidor rodando!!!');
 }
 
 bootstrap();
