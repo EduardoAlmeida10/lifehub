f0cecc4d0b9ad68dd7d0e8eebd817c5d9ee8c29d
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-02-12 11:17:55 -0300

    chore: prettier format

--- api-lifehub/src/app.module.ts
+++ api-lifehub/src/app.module.ts
@@ -1,25 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('LIFEHUB_DB_HOST'),
-        port: Number(config.get<number>('LIFEHUB_DB_PORT')),
-        username: config.get<string>('LIFEHUB_DB_USER'),
-        password: config.get<string>('LIFEHUB_DB_PASSWORD'),
-        database: config.get<string>('LIFEHUB_DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.LIFEHUB_DB_HOST,
+      port: Number(process.env.LIFEHUB_DB_PORT),
+      username: process.env.LIFEHUB_DB_USER,
+      password: process.env.LIFEHUB_DB_PASSWORD,
+      database: process.env.LIFEHUB_DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // em produção colocar coma false
     }),
+    UsersModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
