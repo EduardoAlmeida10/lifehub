12cb4fc5c02e5f1d5398af3a48bebaf9261fdfbd
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-02-25 21:09:01 -0300

    feat: user search endpoint

--- api-lifehub/src/users/users.controller.ts
+++ api-lifehub/src/users/users.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +17,11 @@ export class UsersController {
   @Post()
   create(@Body() dto: CreateUserDto) {
     return this.usersService.create(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
