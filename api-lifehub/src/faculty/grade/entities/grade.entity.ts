ff8676192e8b63aee834dda98af8fee1be022556
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-03-03 10:20:16 -0300

    feat: entity grade create

--- api-lifehub/src/faculty/grade/entities/grade.entity.ts
+++ api-lifehub/src/faculty/grade/entities/grade.entity.ts
@@ -1,1 +1,33 @@
-export class Grade {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { GradeType } from '../enums/grade-type.enum';
+
+@Entity('grades')
+export class Grade {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  courseId: string;
+
+  @Column({
+    type: 'enum',
+    enum: GradeType,
+  })
+  type: GradeType;
+
+  @Column('decimal', { precision: 5, scale: 2 })
+  value: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
