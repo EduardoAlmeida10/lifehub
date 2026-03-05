516492ad7ce39c4318ae67454e1e8afd54df2813
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-03-05 10:44:43 -0300

    feat: course creation dto

--- api-lifehub/src/faculty/course/dto/create-course.dto.ts
+++ api-lifehub/src/faculty/course/dto/create-course.dto.ts
@@ -1,1 +1,28 @@
-export class CreateCourseDto {}
+import { IsEnum, IsString, IsArray, IsNotEmpty } from 'class-validator';
+import { Semester } from '../enums/semester.enum';
+import { DayOfWeek } from '../enums/day-of-week.enum';
+
+export class CreateCourseDto {
+  @IsEnum(Semester)
+  semester: Semester;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  professor: string;
+
+  @IsArray()
+  @IsEnum(DayOfWeek, { each: true })
+  daysOfWeek: DayOfWeek[];
+
+  @IsString()
+  schedule: string;
+
+  @IsString()
+  classroom: string;
+
+  @IsString()
+  color: string;
+}
