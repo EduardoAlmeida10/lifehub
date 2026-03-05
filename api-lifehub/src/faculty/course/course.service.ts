f3dd8faccca5d577d0452e52a8b250966f385327
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-03-05 10:45:27 -0300

    feat: CRUD of the created course module

--- api-lifehub/src/faculty/course/course.service.ts
+++ api-lifehub/src/faculty/course/course.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CourseEntity } from './entities/course.entity';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { RequestUser } from 'src/common/interfaces/request-user.interface';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(
+    @InjectRepository(CourseEntity)
+    private readonly courseRepository: Repository<CourseEntity>,
+  ) {}
+
+  async create(dto: CreateCourseDto, user: RequestUser): Promise<CourseEntity> {
+    try {
+      const course = this.courseRepository.create({
+        ...dto,
+        user: { id: user.userId },
+      });
+
+      return await this.courseRepository.save(course);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  // Os outros métodos NÃO MUDAM (eles já recebem userId: string)
+  async findAll(userId: string): Promise<CourseEntity[]> {
+    return this.courseRepository.find({
+      where: { user: { id: userId } },
+      order: { createdAt: 'DESC' },
+    });
+  }
+
+  async findOne(id: string, userId: string): Promise<CourseEntity> {
+    const course = await this.courseRepository.findOne({
+      where: { id, user: { id: userId } },
+    });
+
+    if (!course) {
+      throw new NotFoundException(`Course with ID ${id} not found`);
+    }
+
+    return course;
+  }
+
+  async update(
+    id: string,
+    dto: UpdateCourseDto,
+    userId: string,
+  ): Promise<CourseEntity> {
+    const course = await this.findOne(id, userId);
+
+    Object.assign(course, dto);
+
+    return this.courseRepository.save(course);
+  }
+
+  async remove(id: string, userId: string) {
+    const course = await this.findOne(id, userId);
+
+    return this.courseRepository.remove(course);
+  }
+}
