348eefd2e6e5b6367118fc77396635f00a8224c3
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-03-06 14:16:38 -0300

    feat: create task dto completed

--- api-lifehub/src/faculty/task/dto/create-task.dto.ts
+++ api-lifehub/src/faculty/task/dto/create-task.dto.ts
@@ -1,1 +1,31 @@
-export class CreateTaskDto {}
+import {
+  IsEnum,
+  IsString,
+  IsNotEmpty,
+  IsUUID,
+  IsDateString,
+} from 'class-validator';
+import { TaskStatus } from '../enums/task-status.enum';
+import { TaskPriority } from '../enums/task-priority.enum';
+
+export class CreateTaskDto {
+  @IsUUID()
+  courseId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsDateString()
+  dueDate: string;
+
+  @IsEnum(TaskStatus)
+  status: TaskStatus = TaskStatus.PENDING;
+
+  @IsEnum(TaskPriority)
+  priority: TaskPriority = TaskPriority.MEDIUM;
+}
