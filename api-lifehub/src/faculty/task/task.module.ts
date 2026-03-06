d38a73d8c39dbb0efb93943931df342c7ff9adcb
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-03-06 14:18:12 -0300

    feat: added course entity in task module

--- api-lifehub/src/faculty/task/task.module.ts
+++ api-lifehub/src/faculty/task/task.module.ts
@@ -3,9 +3,10 @@ import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskEntity } from './entities/task.entity';
+import { CourseEntity } from '../course/entities/course.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskEntity])],
+  imports: [TypeOrmModule.forFeature([TaskEntity, CourseEntity])],
   controllers: [TaskController],
   providers: [TaskService],
 })
