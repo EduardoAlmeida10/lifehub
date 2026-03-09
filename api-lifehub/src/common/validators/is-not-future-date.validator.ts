ac73eba93548853924151f862ae72c0fb968ab5c
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-03-09 09:16:03 -0300

    feat: verify that the date is not in the future.

---
+++ api-lifehub/src/common/validators/is-not-future-date.validator.ts
@@ -0,0 +1,28 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidationArguments,
+} from 'class-validator';
+
+export function IsNotFutureDate(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isNotFutureDate',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: string | Date, args: ValidationArguments) {
+          if (!value) return true;
+          const inputDate = new Date(value);
+          const today = new Date();
+          today.setHours(23, 59, 59, 999);
+          return inputDate <= today;
+        },
+        defaultMessage(args: ValidationArguments) {
+          return 'Date cannot be in the future';
+        },
+      },
+    });
+  };
+}
