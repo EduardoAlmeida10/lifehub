0293aa02f37ff61f0236cf1b132f35f1ea3b9e57
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-03-12 14:07:25 -0300

    feat: grid creation DTO

--- api-lifehub/src/faculty/grade/dto/create-grade.dto.ts
+++ api-lifehub/src/faculty/grade/dto/create-grade.dto.ts
@@ -1,1 +1,15 @@
-export class CreateGradeDto {}
+import { IsEnum, IsNumber, IsUUID, Max, Min } from 'class-validator';
+import { GradeType } from '../enums/grade-type.enum';
+
+export class CreateGradeDto {
+  @IsUUID()
+  courseId: string;
+
+  @IsEnum(GradeType)
+  type: GradeType;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  @Max(10)
+  value: number;
+}
