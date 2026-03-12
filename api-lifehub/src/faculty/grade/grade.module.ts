57ec3107735281ac8869b8c739eedc40e72f2dd1
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-03-12 14:07:57 -0300

    feat: add course entity in grade module

--- api-lifehub/src/faculty/grade/grade.module.ts
+++ api-lifehub/src/faculty/grade/grade.module.ts
@@ -3,9 +3,10 @@ import { GradeService } from './grade.service';
 import { GradeController } from './grade.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GradeEntity } from './entities/grade.entity';
+import { CourseEntity } from '../course/entities/course.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GradeEntity])],
+  imports: [TypeOrmModule.forFeature([GradeEntity, CourseEntity])],
   controllers: [GradeController],
   providers: [GradeService],
 })
